9e74107661a9f39482723bd55194e92c9ff0ce88
Deborah'syemojarepo <deborah.aina@outlook.com>
2020-11-08 21:45:27 -0500

    Final edits

--- config.py
+++
@@ -1,4 +0,0 @@
-gkey = 'AIzaSyATWXjITly_haq-ZYI8fYlDXft3Ybeo11s'
-api_key ="pk.eyJ1IjoiZGVib3JhaGFpbmEiLCJhIjoiY2p1amQ2djl5MDVtcTN5czZ5ZDNhZHhxYiJ9.pbqZG9FjB-vF1XeRUYIgRQ"
-username = "Daina92"
-pwd = "Wfamvzko8zxMbVZ1VZ7B"
